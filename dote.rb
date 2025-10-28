3e4bf2a7fdc4ed7862f50f18dca92bc76ececa5d
YOMIRU <yomiru.system@gmail.com>
2025-10-28 11:17:03 +0900

    Update homepage and URL in Dote class

--- dote.rb
+++ dote.rb
@@ -1,7 +1,7 @@
 class Dote < Formula
   desc "dot e command"
-  homepage "https://github.com/yomi-ru/brew-MovingMyaku"
-  url "https://raw.githubusercontent.com/yomi-ru/brew-MovingMyaku/main/dote.c"
+  homepage "https://github.com/yomi-ru/homebrew-MovingMyaku"
+  url "https://raw.githubusercontent.com/yomi-ru/homebrew-MovingMyaku/main/dote.c"
   sha256 "002a07014fc33d979bffd712228c569885f17a339464c81c0625cb45cb5ee17c"
   version "1.0.0"
 
