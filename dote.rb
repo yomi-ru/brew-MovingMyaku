7866cb4661d6b81f805a38377fa4016ab473ddfd
YOMIRU <yomiru.system@gmail.com>
2025-10-28 11:01:51 +0900

    Update URL and SHA256 for dote.rb
    v1.0.0

--- dote.rb
+++ dote.rb
@@ -1,8 +1,9 @@
 class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/yomi-ru/brew-MovingMyaku"
-  url "https://raw.githubusercontent.com/kmc2400/homebrew-dote/main/dote.c"
-  sha256 "PUT_SHA256_OF_DOTE.C_HERE"
+  url "https://raw.githubusercontent.com/yomi-ru/brew-MovingMyaku/main/dote.c"
+  sha256 "002a07014fc33d979bffd712228c569885f17a339464c81c0625cb45cb5ee17c"
+  version "1.0.0"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
